a38481f0cd9cf57595c3412984dc7410def3b92b
MontanDeev <ms.barrales.developerbackend@gmail.com>
2024-09-11 14:47:32 -0600

    agregué el paquete de desarrollo

---
+++ src/main/java/com/example/ElMapacheBigoton/ElMapacheBigotonApplication.java
@@ -0,0 +1,13 @@
+package com.example.ElMapacheBigoton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElMapacheBigotonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElMapacheBigotonApplication.class, args);
+	}
+
+}
