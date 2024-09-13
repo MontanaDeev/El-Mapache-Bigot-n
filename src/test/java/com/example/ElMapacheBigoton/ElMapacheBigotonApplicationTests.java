3f8cfdf49baa08058abca77c1a8246e592083c87
MontanDeev <ms.barrales.developerbackend@gmail.com>
2024-09-13 12:02:07 -0600

    carpetas

---
+++ src/test/java/com/example/ElMapacheBigoton/ElMapacheBigotonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ElMapacheBigoton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElMapacheBigotonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
