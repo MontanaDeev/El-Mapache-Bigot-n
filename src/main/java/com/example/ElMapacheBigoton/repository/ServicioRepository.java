21ebf14bfd21de07c2640bd6c99673c1d55416d5
EduardoH <eduardoisidroh13@gmail.com>
2024-09-13 23:07:28 -0600

    Creacion de ServicioRepository

---
+++ src/main/java/com/example/ElMapacheBigoton/repository/ServicioRepository.java
@@ -0,0 +1,7 @@
+package com.example.ElMapacheBigoton.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.ElMapacheBigoton.model.Servicio;
+public interface ServicioRepository extends CrudRepository<Servicio, Long>{
+
+}
