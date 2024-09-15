ab7a07039fce68c455d258592f777f84718e9bbd
EduardoH <eduardoisidroh13@gmail.com>
2024-09-14 21:31:47 -0600

    Refactorizar importación en ServicioController

--- src/main/java/com/example/ElMapacheBigoton/model/Servicio.java
+++ src/main/java/com/example/ElMapacheBigoton/model/Servicio.java
@@ -1,12 +1,12 @@
 package com.example.ElMapacheBigoton.model;
 
-import jakarta.persistence.CascadeType;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+
 import jakarta.persistence.Table;
 
 @Entity
